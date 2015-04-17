46628e7a9e1eec18d8740762e08f8b5196a8857c
shappy082 <papawich.int@gmail.com>
2015-04-17 20:33:42 +0700

    *07*
    - เปลี่ยนการแสดงเลือดจากตัวเลขเป็นแถบสี
    - บอกสถานะของแถบเลือดด้วยสีเขียว เหลือง แดง ตามระดับเลือด

--- f2/spw/GamePanel.java
+++ f2/spw/GamePanel.java
@@ -11,7 +11,7 @@ import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
 	
-	private BufferedImage bi;	
+	private BufferedImage bi;
 	Graphics2D big;
 	ArrayList<Sprite> sprites = new ArrayList<Sprite>();
 
@@ -22,22 +22,19 @@ public class GamePanel extends JPanel {
 	}
 
 	public void updateGameUI(GameReporter reporter){
+		int barHP = reporter.getHP()/200;
+		StringBuilder hp = new StringBuilder();
+
 		big.clearRect(0, 0, 400, 600);
 		big.setColor(Color.WHITE);
 		big.setFont(new Font ("Helvetica", Font.PLAIN, 12));
 
-		//HP Display
-		big.drawString(String.format("HP: %04d", reporter.getHP()), 20, 20);
-		
 		//Time Display
 		long time = reporter.getTime();
 		big.drawString(String.format("Time: %02d.%02d", time/60, time%60), 20, 40);
 
 		//Score Display
         big.drawString(String.format("%08d", reporter.getScore()), 165, 20);
-
-        //Display text on Spaceship
-        //big.drawString("player", reporter.getPosition().x - 10, reporter.getPosition().y - 10);
 
         //Difficult Displays
         if (reporter.getDifficulty() > 0.7) {
@@ -49,6 +46,26 @@ public class GamePanel extends JPanel {
         else if (reporter.getDifficulty() > 0.0) {
             big.drawString("Difficult: EASY", 280, 20);
         }
+
+		//HP Display
+		big.drawString("HP: ", 20, 20);
+		for (int i = 0; i < barHP; i++) {
+			hp.append("|");
+		}
+		if (barHP > 10) {
+			big.setColor(Color.GREEN);
+		}
+		else if (barHP > 5) {
+			big.setColor(Color.YELLOW);
+		}
+		else if (barHP > 0) {
+			big.setColor(Color.RED);
+		}
+		big.drawString(hp.toString(), 45, 20);
+		hp.delete(0,20);
+
+        //Display text on Spaceship
+        //big.drawString("player", reporter.getPosition().x - 10, reporter.getPosition().y - 10);
 
 		//DRAW Object
         for (Sprite s : sprites) {
